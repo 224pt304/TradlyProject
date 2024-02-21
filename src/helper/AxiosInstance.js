d07da87ab00bd7c07f2563e32fde0603f4433e8e
Thai1812 <130356756+Thai1812@users.noreply.github.com>
2024-02-21 16:04:12 +0700

    thai

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -5,7 +5,7 @@ const AxiosInstance = (contentType = 'application/json') => {
     const axiosInstance = axios.create({
         // baseURL: 'https://65c460ffdae2304e92e2818b.mockapi.io/'
         // baseURL: 'http://192.168.43.237:3000/'
-        baseURL: 'http://172.16.77.177:3000/'
+        baseURL: 'http://172.16.92.76:3000/'
     });
 
     axiosInstance.interceptors.request.use(
